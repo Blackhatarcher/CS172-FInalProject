7a9b9dece1616b4cebc2c27c7b79a96dc0b7c5e6
christianwilliams20 <christianwilliams20@LAB-SCI-214-07>
2016-12-08 15:21:15 -0800

    writing out mehtods for account

--- CashKeeper/CashKeeper/Account.cpp
+++ CashKeeper/CashKeeper/Account.cpp
@@ -1,12 +1,30 @@
 #include "stdafx.h"
 #include "Account.h"
 
+//defualt account constructor that makes an account with a name of "account" and balance 0
+Account::Account() {
+	
+}
+//create and account of balance b and name n
+Account::Account(double b, string n) {
 
-Account::Account()
-{
 }
 
+Account::~Account() {
 
-Account::~Account()
-{
+}
+double Account:: getBalance() {
+
+}
+void Account:: changeBalance() {
+
+}
+string Account:: getName() {
+
+}
+void Account:: transfer(double x, Account a) {
+
+}
+void Account:: update() {
+
 }
--- CashKeeper/CashKeeper/Account.h
+++ CashKeeper/CashKeeper/Account.h
@@ -1,8 +1,25 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include "Record.h"
+using namespace std;
+
 class Account
 {
+protected:
+	vector<Record> records;
+	double balance;
+	string name;
+
 public:
 	Account();
+	Account(double b, string n);
 	~Account();
+	double getBalance();
+	void changeBalance();
+	string getName();
+	void transfer(double x, Account a);
+	void update();
 };
 
